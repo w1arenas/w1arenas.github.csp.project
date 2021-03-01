40803fecb49f5d03090daa7aa95e5c797e126719
Walter Arenas <walter@Walters-MacBook-Air.local>
2021-03-01 12:39:31 -0500

    Successfuly posted first entry to mongo database using mongoose.

--- models/cspschema.js
+++ models/cspschema.js
@@ -2,6 +2,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
+const cspSchema = new Schema ({
+    name: {type: String, required: true},
+    council: {type: String, required: true},
+    issue: String,
+    //body: String,
+    //img: {type: String, required: true},
+    //comments: [{body: String}]
+}, {timestamps: true});
 
+const Csp = mongoose.model("Csp", cspSchema);
 
 /////////////// Export the model to make it accessible in server.js
+module.exports = Csp; 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /////////////// DEPENDENCIES
 const express = require("express");	
 const mongoose = require("mongoose");
+const Csp = require("./models/cspschema.js")
 
 /////////////// GLOBAL CONFIGURATION
 const mongoURI = "mongodb://localhost:27017/" + "csps"
@@ -31,10 +32,31 @@ const collectionController = require("./controllers/cspcollection.js")
 /////////////// MIDDLEWARE
 APP.use(collectionController)
 
-	/////////////// DATABASE CONFIGURATION
+/////////////// DATABASE CONFIGURATION
 
 
-	/////////////// LISTENER
+
+/////////////// ITEM CREATION - testing
+const myFirstCspEntry = {
+    name: "Alapaha Area Council",
+    council: "Alapaha Area Council",
+    issue: "T1b"
+}
+
+Csp.create(myFirstCspEntry, (error, cspItem) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(cspItem)
+    }
+    db.close
+})
+
+
+
+
+
+/////////////// LISTENER
 APP.listen(PORT, () => {
 	console.log("Listening on PORT: " + PORT)
 })
