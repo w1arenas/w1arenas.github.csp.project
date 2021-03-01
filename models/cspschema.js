8c235dc82dbf83b69cc35c18cdae4f77884bc230
Walter Arenas <walter@Walters-MacBook-Air.local>
2021-03-01 00:50:08 -0500

    Built mongoose functionality. Started building the schema.

---
+++ models/cspschema.js
@@ -0,0 +1,7 @@
+/////////////// DEPENDENCIES
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+
+
+/////////////// Export the model to make it accessible in server.js
--- server.js
+++ server.js
@@ -6,8 +6,19 @@ const mongoose = require("mongoose");
 const mongoURI = "mongodb://localhost:27017/" + "csps"
 const db = mongoose.connection;
 
+/////////////// CONNECTION ERROR/SUCCESS
+db.on("error", (err) => console.log(err.message + "is Mongo not running"));
+db.on("connected", () => console.log("Mongo connected: " + mongoURI));
+db.on("disconnected", () => console.log("Mongo disconnected"));
+
 /////////////// CONNECT TO MONGO
-mongoose.connect(mongoURI)
+mongoose.connect(mongoURI, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("The connection with Mongo is established.")
+});
 
 /////////////// APP CONFIGURATION
 const APP = express();
@@ -28,3 +39,4 @@ APP.listen(PORT, () => {
 	console.log("Listening on PORT: " + PORT)
 })
 
+setTimeout(() => { db.close(); }, 5000)
