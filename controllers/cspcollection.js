b078cae25561e3dcfe1ed46a2c01311404c0fafb
Walter Arenas <walter@Walters-MacBook-Air.local>
2021-03-01 20:23:16 -0500

    Added SEED route.

--- controllers/cspcollection.js
+++ controllers/cspcollection.js
@@ -13,16 +13,41 @@ const Csp = require("../models/cspschema.js")
 /////////////// ROUTES
 
 //seed route
-
+ROUTER.get("/cspcollection/seed", (req, res) => {
+	Csp.create([
+		{
+			image: "some image",
+			council: "Alapaha Area Council",
+			issue: "T1b"
+		},
+		{
+			image: "some image",
+			council: "Atlanta Area Council",
+			issue: "T11a"
+		},
+		{
+			image: "some image",
+			council: "Central Georgia Council",
+			issue: "T2"
+		},		
+		{
+			image: "some image",
+			council: "Chattahoochee Council",
+			issue: "S4"
+		}
+	], (error, data) => {
+		res.redirect("/cspcollection");
+	})
+})
 
 //index route
 ROUTER.get("/cspcollection", (req, res) => {
     Csp.find({}, (error, csps) => {
         res.render("index.ejs", {
             allCsps: csps
-        })
-    })
-})
+        });
+    });
+});
 
 //new route
 ROUTER.get("/cspcollection/new", (req, res) => {
@@ -42,11 +67,11 @@ ROUTER.post('/cspcollection', (req, res) => {
 // show route
 ROUTER.get("/cspcollection/:id", (req, res) => {
     Csp.findById(req.params.id, (error, foundCsp) => {
-	res.render("show.ejs", {
-		csp: foundCsp
-	}) 
+		res.render("show.ejs", {
+			csp: foundCsp
+		});
 		
-    })
+    });
 });
 
 module.exports = ROUTER;
